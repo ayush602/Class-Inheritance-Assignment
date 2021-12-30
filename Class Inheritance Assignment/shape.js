d7f9cbe42638bc5bf11c9c8a589193283f137f67
ayush602 <94966497+ayush602@users.noreply.github.com>
2021-12-30 06:39:12 -0800

    Add files via upload

---
+++ Class Inheritance Assignment/shape.js
@@ -0,0 +1,14 @@
+// export class using module.exports
+module.exports = Circle;
+ // declare class
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
+
+     }
+     calculateArea() {
+
+     }
+}
